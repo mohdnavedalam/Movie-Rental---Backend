a591904fac4675e4505e9895990024fc81f5c651
Mohd Naved Alam <mohdnavedalam87@gmail.com>
2020-10-12 02:34:09 +0530

    Exercise - 5

--- Movie Rental - Backend/Program.cs
+++ Movie Rental - Backend/Program.cs
@@ -7,7 +7,7 @@ namespace Movie_Rental___Backend
     {
         static void Main(string[] args)
         {
-            //Exercise_3.ExecuteExercise_3();
+            Exercise_3.ExecuteExercise_3();
 
             //Exercise 5:
             //Task - 1
@@ -17,28 +17,32 @@ namespace Movie_Rental___Backend
             // have the Id for each genre. If you're building a WPF application, this 
             // GenreId is already in the memory. If you're building an ASP.NET MVC application,
             // the GenreId is posted with the request and you can set it here. 
-            //Exercise_5.AddVideo(new Video
-            //{
-            //    Name = "Terminator 1",
-            //    GenreID = 2,
-            //    Classification = Classification.Silver,
-            //    ReleaseDate = new DateTime(1984, 10, 26)
-            //});
+            Exercise_5.AddVideo(new Video
+            {
+                Name = "Terminator 1",
+                GenreID = 2,
+                Classification = Classification.Silver,
+                ReleaseDate = new DateTime(1984, 10, 26)
+            });
 
             // Task - 2
-            //Exercise_5.AddTags("classics", "drama");
+            Exercise_5.AddTags("classics", "drama");
 
             //Task - 3
-            //Exercise_5.AddTagsToVideo(1, "classics", "drama", "comedy");
+            Exercise_5.AddTagsToVideo(1, "classics", "drama", "comedy");
 
             //Task - 4
-            //Exercise_5.RemoveTagsFromVideo(1, "comedy"); // Can't remove the tag. Pending
+            Exercise_5.RemoveTagsFromVideo(1, "comedy"); // Can't remove the tag. Pending
 
             //Task - 5
-            //Exercise_5.RemoveVideo(1); // Check if the video with the given ID exists
+            Exercise_5.RemoveVideo(1); // Check if the video with the given ID exists
 
             //Task - 6
             Exercise_5.RemoveGenre(2, true);
+
+            // The Exercise - 5 methods need validations.
+            // They are behaving erratically.
+            // Look into this.
         }
     }
 }
